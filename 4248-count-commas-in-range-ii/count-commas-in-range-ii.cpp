c93f175f254ed2e71fa62340e20cf696e4a6c619
Rutvik Tayde <rutviktayde73@gmail.com>
2026-09-11 22:35:42 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (82.91%) - LeetSync

---
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    long long countCommas(long long n) {
+        long long commas=0;
+        if(n<1000){
+            return 0;
+        }
+        if(n>=1000){
+            commas=n-999;
+        }
+        if(n>=1000000){
+            commas+=(n-999999);
+        }
+        if(n>=1000000000){
+            commas+=n-999999999;
+        }
+        if(n>=1000000000000){
+            commas+=n-999999999999;
+        }
+        if(n>=1000000000000000){
+            commas+=n-999999999999999;
+        }
+
+        return commas;
+    }
+};
